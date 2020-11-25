e93222398777b93256ba47c819e1ffe35bfd3e86
leonie <leonie.strasser@yahoo.de>
2020-11-25 09:52:11 +0100

    first Labyrinth
    (+) third person movement
    (+) player
    (+) snackpoint-systhem

---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public int snackpoints = 0;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GetSnackpoints.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+
+public class GetSnackpoints : MonoBehaviour
+{
+    public GameManagerScript gms;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Snackpoint")
+        {
+            gms.snackpoints++;
+            Destroy(other.gameObject);
+        }
+    }
+}
