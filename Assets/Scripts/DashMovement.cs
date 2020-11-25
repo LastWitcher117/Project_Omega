24b61e8b700cbc92508c71b3ab26e3f282ad910c
Tim Jahnke <timjahnke@t-online.de>
2020-11-25 15:39:55 +0100

    Dash Movement
    [+] Added Dash Movement

---
+++ Assets/Scripts/DashMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashMovement : MonoBehaviour
+{
+    ThirdPersonMovement moveScript;
+
+    public float dashSpeed;
+    public float dashTime;
+    public CharacterController controller;
+    public bool isDashing = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveScript = GetComponent<ThirdPersonMovement>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) //Input Leftclick höhö
+        {
+            
+            StartCoroutine(Dash());          
+            
+            if (isDashing == true)
+            {
+                StartCoroutine(Timer());
+            }
+        }
+    }
+    IEnumerator Dash()
+    {
+        float startTime = Time.time;
+        if (isDashing == false)
+        {         
+            while (Time.time < startTime + dashTime) //Dash movement 
+            {
+                moveScript.controller.Move(moveScript.moveDir * dashSpeed * Time.deltaTime);
+                isDashing = true;
+                yield return null;
+                
+            }
+        }
+        
+    }
+
+    IEnumerator Timer() //Couldown timer
+    {    
+        yield return new WaitForSeconds(4.0f);
+        isDashing = false;
+    }
+
+}
--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -9,29 +9,34 @@ public class ThirdPersonMovement : MonoBehaviour
     public float speed = 6f;
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
+    public Vector3 moveDir;
 
-    // Start is called before the first frame update
+  
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
+
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        float horitontal = Input.GetAxisRaw("Horizontal");
+        float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horitontal, 0, vertical).normalized;
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
 
-        if(direction.magnitude >= 0.1f) //magnitude is the length of the direction vector
+      
+        if (direction.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y; //ROTATION//get the anggle in the klick and view direction
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime); //ROTATION//smooth the rotation by directionchange
-            transform.rotation = Quaternion.Euler(0f, angle, 0f); //ROTATION//rotate the character in the right direction by using angle
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
 
-            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f )*Vector3.forward;
-            controller.Move(moveDirection.normalized * speed * Time.deltaTime); //moveFunktion
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
+            moveDir = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
+
     }
 }
