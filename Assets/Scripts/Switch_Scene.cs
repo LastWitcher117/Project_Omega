575fa4bdccbd491bc64ac3294b7a57a833a49fa7
Filip <LastWitcher@outlook.de>
2020-12-09 13:10:02 +0100

    In Game UI
    [+] Score UI

--- Assets/Scripts/Switch_Scene.cs
+++ Assets/Scripts/Switch_Scene.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Switch_Scene : MonoBehaviour
@@ -9,19 +10,31 @@ public class Switch_Scene : MonoBehaviour
 
     public int nextSceneToLoad;
 
-    IEnumerator OnTriggerEnter(Collider other)
+    public GameObject ScorePoints;
+    public GameManagerScript gm;
+
+    public void OnTriggerEnter(Collider other)
     {
         You_Win_Screen.enabled = true;
         FindObjectOfType<AudioManager>().Play("WinSound");
         Time.timeScale = 0f;
-        float pauseEndTime = Time.realtimeSinceStartup + 2.5f;
+
+        /*float pauseEndTime = Time.realtimeSinceStartup + 2.5f;
         while (Time.realtimeSinceStartup < pauseEndTime)
         {
             yield return 0;
         }
-        Time.timeScale = 1f;
+        Time.timeScale = 1f;*/
+
         FindObjectOfType<AudioManager>().Play("Theme");
-        SceneManager.LoadScene(0);
+        //SceneManager.LoadScene(0);
+        Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void Update()
+    {
+        ScorePoints.GetComponent<Text>().text = gm.snackpoints.ToString();
+
     }
 }
