88b66756fc0f2a0b1007adf1c36b5eb524951bf0
leonie <leonie.strasser@yahoo.de>
2020-12-10 21:01:30 +0100

    Added Get_Treasure skript
    [+] Get_Treasure Script
    [+] Test_Treasure Prefab
    [+] New Tag "Treasure"

---
+++ Assets/Scripts/GetTreasure.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetTreasure : MonoBehaviour
+{
+    public GameManagerScript gms;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Treasure")
+        {
+            gms.snackpoints = gms.snackpoints + 1000;
+            FindObjectOfType<AudioManager>().Play("SnackSound");
+            Destroy(other.gameObject);
+        }
+    }
+}
