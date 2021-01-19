8718a44251c88277252c21400b3c16ccb0e682f0
leonie <leonie.strasser@yahoo.de>
2021-01-19 19:32:34 +0100

    Added Orientation Fire Prefab
    [+] Added a fire placeholder Prefab
     [?] Fire starts OnTriggerEnter automaticly so the player knows better what lamps he passed and what areas are new

---
+++ Assets/Scripts/Orientation_Fire_Script.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orientation_Fire_Script : MonoBehaviour
+{
+
+   public GameObject VFX_Fire;
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            VFX_Fire.SetActive(true);
+        }
+    }
+}
