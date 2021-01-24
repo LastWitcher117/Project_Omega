b1133eeb5b13d5f60231867f2d01db87c46573e6
Tim Jahnke <timjahnke@t-online.de>
2021-01-24 19:12:14 +0100

    Added "Sprite Turner"
    [+] Sprites turn now with camera

---
+++ Assets/Scripts/SpriteTurner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteTurner : MonoBehaviour
+{
+
+    /*/-------------------------------------------------------------------------------------------------------------------------------------------------/*/
+    //Sprite/Material/whatever turning to camera
+    public Transform target;
+
+    public float ObjectTurningSpeed;
+    /*/-------------------------------------------------------------------------------------------------------------------------------------------------/*/
+
+
+    void Update()
+    {
+        /*/-------------------------------------------------------------------------------------------------------------------------------------------------/*/
+        var step = ObjectTurningSpeed * Time.deltaTime;
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, target.rotation, step);
+        /*/-------------------------------------------------------------------------------------------------------------------------------------------------/*/
+    }
+}
