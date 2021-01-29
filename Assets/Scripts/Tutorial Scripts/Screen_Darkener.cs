3bc556c3153adace4594de65c6d0cc4df6571c81
Tim Jahnke <timjahnke@t-online.de>
2021-01-29 18:09:27 +0100

    Added Darker Screen on "World Edge" in Tutorial
    [+] Added Dark Controller Script "Screen_Darkener"
    [*] Fixed holes in Tutorial and Collider issues

---
+++ Assets/Scripts/Tutorial Scripts/Screen_Darkener.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Screen_Darkener : MonoBehaviour
+{
+
+    public Image ScreenDarkener;
+    //public float Darkness = 0f;
+    public bool isInTrigger = false;
+
+    public float Dark_Light_Value = 0.1f;
+    public float Timing = 0.5f;
+
+    public Color Darkness;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        isInTrigger = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isInTrigger = false;
+    }
+
+    private void Update()
+    {
+
+       
+
+        if (isInTrigger == true)
+        {
+            InvokeRepeating("DarkerScreen", Timing, Timing);
+
+        }
+        else
+        {
+            InvokeRepeating("LighterScreen", Timing, Timing);
+        }
+
+
+
+    }
+
+    void DarkerScreen()
+    {
+        if (Darkness.a <= 0.6)
+        {
+            CancelInvoke("DarkerScreen");
+
+            Darkness.a = Darkness.a + Dark_Light_Value;
+
+            ScreenDarkener.color = Darkness;
+        }
+    }
+
+    void LighterScreen()
+    {
+        if (Darkness.a >= 0)
+        {
+            CancelInvoke("LighterScreen");
+
+            Darkness.a = Darkness.a - Dark_Light_Value;
+
+            ScreenDarkener.color = Darkness;
+        }
+    }
+
+}
