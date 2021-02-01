70ea759c149aea86d307832981261b8ddc3fbb77
Tim Jahnke <timjahnke@t-online.de>
2021-02-01 18:43:07 +0100

    Changed Attack Animation added Zoom Out Feature Maps Changed Tower Materials
    [+] Added Zoom out 
    [*] Tower Teleporter Materials cahnged 
    [*] Changed Attack Animation

---
+++ Assets/Scripts/CameraMap.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMap : MonoBehaviour
+{
+
+    public GameObject vcam;
+    public GameObject MainCamera;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        vcam.SetActive(true);
+        MainCamera.SetActive(false);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        vcam.SetActive(false);
+        MainCamera.SetActive(true);
+    }
+}
