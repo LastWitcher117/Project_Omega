82af28eb776f586e3736b641880e533d1840785f
Filip <LastWitcher@outlook.de>
2021-02-01 14:27:03 +0100

    Random Spawn
    [+] Random Keys
    [+] Random Treasure

--- Assets/Scripts/GetKey.cs
+++ Assets/Scripts/GetKey.cs
@@ -9,6 +9,8 @@ public class GetKey : MonoBehaviour
     public ExitWay EW;
     public GameManagerScript gms;
 
+    public Random_Spawn rs;
+
     private void OnTriggerEnter(Collider other)
     {
         EnterSignal = true;
@@ -16,7 +18,24 @@ public class GetKey : MonoBehaviour
 
         EnterSignal = false;
         transform.position = transform.position + new Vector3(0f, -10f, 0f);
-        EW.Lights.SetActive(true);
+        //EW.Lights.SetActive(true);
+
+        if (rs.spawnLocation == 1)
+        {
+            rs.way1.SetActive(true);
+        }
+        if (rs.spawnLocation == 2)
+        {
+            rs.way2.SetActive(true);
+        }
+        if (rs.spawnLocation == 3)
+        {
+            rs.way3.SetActive(true);
+        }
+        if (rs.spawnLocation == 4)
+        {
+            rs.way4.SetActive(true);
+        }
     }
 
 }
