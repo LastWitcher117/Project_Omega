d806e5278af0de573abe131861d5acb80909052e
Tim Jahnke <timjahnke@t-online.de>
2021-02-02 09:51:46 +0100

    Added new Letter to Pacsy and fixed minor bugs

--- Assets/Scripts/Tutorial Scripts/Letter_to_Pacsy.cs
+++ Assets/Scripts/Tutorial Scripts/Letter_to_Pacsy.cs
@@ -9,6 +9,8 @@ public class Letter_to_Pacsy : MonoBehaviour
 
     public AnimationController AC;
 
+    public Pause_Menu PM;
+
     public bool InLetter = false;
 
     public bool Switch = false;
@@ -17,6 +19,9 @@ public class Letter_to_Pacsy : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        PM.GameIsPaused = true;
+        Cursor.lockState = CursorLockMode.Locked;
+
         if (Switch == false)
         {
             LetterToPacsy.gameObject.SetActive(true);
@@ -39,6 +44,7 @@ public class Letter_to_Pacsy : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space) && InLetter == true)
         {
+
             AC.inTutorial = false;
             LetterToPacsy.gameObject.SetActive(false);
 
@@ -50,7 +56,6 @@ public class Letter_to_Pacsy : MonoBehaviour
             //Stop/break of the Audio here | Yes even if it interreupts the audio ;) 
             //cause the player is able to walk inside the letter again if he skips it accidentally :D
         }
-
     }
 
     private void Start()
