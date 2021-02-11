b9eedfdfbee860753e7b786b4c6ae79a01bc8f4b
Tim Jahnke <timjahnke@t-online.de>
2021-02-11 11:43:56 +0100

    Fixed Credits Bug again and added TrailerCameras

---
+++ Assets/Scripts/OnlyForTrailer/TrailerCameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailerCameraController : MonoBehaviour
+{
+
+    public GameObject Lower_Perspective;
+    public GameObject Zoom_Out;
+    public GameObject Top_Down;
+    public GameObject Top_Down2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
