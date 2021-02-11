edda0569195ace1159073c6c0057875777664963
Tim Jahnke <timjahnke@t-online.de>
2021-02-11 14:05:37 +0100

    Shortcuts for Trailer and a lot of Cameras

--- Assets/Scripts/OnlyForTrailer/TrailerCameraController.cs
+++ Assets/Scripts/OnlyForTrailer/TrailerCameraController.cs
@@ -4,21 +4,80 @@ using UnityEngine;
 
 public class TrailerCameraController : MonoBehaviour
 {
+    public GameObject ThirdPersonCamera;
 
     public GameObject Lower_Perspective;
     public GameObject Zoom_Out;
     public GameObject Top_Down;
     public GameObject Top_Down2;
+    public GameObject ThirdPersonStatic;
+    public GameObject GhostCam;
+    public GameObject GhostMaskCam;
+    public GameObject SnakeCam;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+       
+        if(Input.GetKeyDown(KeyCode.Keypad0))
+        {
+            ThirdPersonCamera.SetActive(true);
+            Lower_Perspective.SetActive(false);
+            Zoom_Out.SetActive(false);
+            Top_Down.SetActive(false);
+            Top_Down2.SetActive(false);
+            ThirdPersonStatic.SetActive(false);
+            GhostCam.SetActive(false);
+            SnakeCam.SetActive(false);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            ThirdPersonCamera.SetActive(false);
+            Lower_Perspective.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            ThirdPersonCamera.SetActive(false);
+            Zoom_Out.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            ThirdPersonCamera.SetActive(false);
+            Top_Down.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Keypad4))
+        {
+            ThirdPersonCamera.SetActive(false);
+            Top_Down2.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Keypad5))
+        {
+            ThirdPersonCamera.SetActive(false);
+            ThirdPersonStatic.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Keypad6))
+        {
+            ThirdPersonCamera.SetActive(false);
+            GhostCam.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Keypad7))
+        {
+            ThirdPersonCamera.SetActive(false);
+            GhostMaskCam.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Keypad9))
+        {
+            ThirdPersonCamera.SetActive(false);
+            SnakeCam.SetActive(true);
+        }
+
     }
 }
